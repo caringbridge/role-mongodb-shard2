dc46613afd1e833effe4f1e11b063aa1f07bf8c7
Stephen Van Zee <svanzee@caringbridge.org>
2015-06-29 11:09:33 -0500

    added replset attr to shard attr

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,3 +17,7 @@ default['mongodb']['ruby_gems'] = {
   :mongo => '1.12.0',
   :bson_ext => '1.12.0'
 }
+
+default['mongodb']['config']['replSet'] = 'rs_shard2'
+default['mongodb']['is_replicaset'] = true
+default['mongodb']['shard_name'] = 'shard2'
